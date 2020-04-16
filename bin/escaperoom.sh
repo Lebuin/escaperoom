3b75df7e8007d721d25cb1276a38391e0204c379
Seppe Lenders <seppe.lenders@gmail.com>
2020-04-16 21:06:17 +0200

    bugfixes

--- bin/escaperoom.sh
+++ bin/escaperoom.sh
@@ -3,9 +3,10 @@
 cd /opt/escaperoom
 docker-compose up -d
 
-until curl http://localhost:8081; do
+until curl http://localhost:8081 > /dev/null; do
     sleep 1
 done
 
 chromium-browser --start-fullscreen 'http://localhost:8081' &
 disown
+echo
--- build/install.sh
+++ build/install.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
+# Install general dependencies
+apt-get install -y libffi-dev libssl-dev
+
 # Install rfid-reader.py dependencies
 pip3 install mfrc522 pyautogui
 
 # Don't show the setup and ssh warning
 rm /etc/xdg/autostart/piwiz.desktop  # Hide the setup
-apt purge libpam-chksshpwd
+apt-get purge -y libpam-chksshpwd
 
 # Install docker and docker-compose
-curl -fsSL https://get.docker.com -o get-docker.sh
-sh get-docker.sh
+curl -sSL https://get.docker.com | sh
 usermod -aG docker pi
-curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+pip3 install docker-compose
 
 
 # Install our own files
 cp escaperoom.desktop /etc/xdg/autostart
+
 cp rfid-reader.service /lib/systemd/system
+systemctl daemon-reload
+systemctl enable rfid-reader
